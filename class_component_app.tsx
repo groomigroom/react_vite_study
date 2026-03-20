d1d978daa005ff3d8fe5d8b7ec6228a5161127da
groomigroom <googjohn0143@gmail.com>
2026-03-20 16:39:47 +0900

    Create class_component_app.tsx

---
+++ class_component_app.tsx
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a href="https://www.google.com">
+            <p>구글로 가요</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
+}
