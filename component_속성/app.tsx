aa9c0da49ee55a554a1c553101f6d92bcc1f748c
groomigroom <googjohn0143@gmail.com>
2026-03-22 11:49:55 +0900

    Create app.tsx

---
+++ component_속성/app.tsx
@@ -0,0 +1,11 @@
+import ClassComponent from './component/component';
+
+function App() {
+  return (
+    <div>
+      <ClassComponent href='https://www.naver.com' text='groomi'/>
+    </div>
+  )
+}
+
+export default App
