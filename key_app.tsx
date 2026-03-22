8b2cacb461f41bf8b86b4b17157b9803f53de421
groomigroom <googjohn0143@gmail.com>
2026-03-23 07:06:17 +0900

    Update key_app.tsx

--- key_app.tsx
+++ key_app.tsx
@@ -8,14 +8,31 @@
 // }
 // #####
 
-const items = ["goo", "goorem", "googooeee"];
+// const items = ["goo", "goorem", "googooeee"];
+
+// export default function App() {
+//   return (
+//     <div>
+//       {items.map((item) => (
+//         <p key={item}>{item}</p>
+//       ))}
+//     </div>
+//   );
+// }
+
+// #####
 
 export default function App() {
+  const texts = ['hello', 'world'].map((text, index) =>
+  <p key={index}>{text}</p>)
+
   return (
     <div>
-      {items.map((item) => (
-        <p key={item}>{item}</p>
-      ))}
+      {texts}
     </div>
-  );
+  )
 }
+
+/*
+React에서 map() 함수는 배열 데이터를 기반으로 HTML 요소(JSX) 목록을 반복적으로 렌더링할 때 사용하는 핵심 내장 함수입니다. 기존 배열을 수정하지 않고, 각 요소를 새로운 JSX 컴포넌트로 변환하여 새로운 배열을 반환하며, 데이터에 따라 UI를 동적으로 생성하는 데 필수적입니다. 
+*/
