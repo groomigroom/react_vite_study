09dadcb7c4c23d9224760911f8f6e6ab872a020d
groomigroom <googjohn0143@gmail.com>
2026-05-25 22:26:30 +0900

    Create function_type.tsx

---
+++ function_type.tsx
@@ -0,0 +1,25 @@
+function add(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+
+add(1, 2);
+
+function showItems(arr: number[]) {
+  arr.forEach((item) => {
+    console.log("김구름의" + item);
+  });
+}
+
+showItems([1, 2, 3, 4]);
+
+function App() {
+
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
