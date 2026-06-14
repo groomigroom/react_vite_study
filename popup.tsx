5f2d343086985a321dfaa744961516290a8264a8
groomigroom <googjohn0143@gmail.com>
2026-06-15 06:37:42 +0900

    Create popup.tsx

---
+++ popup.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+
+const App: React.FC = () => {
+  //Function Component의 약자로, React에서 함수형 컴포넌트를 정의할 때 사용하는 타입스크립트(TypeScript) 타입입니다. 리액트 패키지(@types/react)에서 기본적으로 제공합니다.
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const openPopup = () => setIsOpen(true);
+  const closePopup = () => setIsOpen(false);
+  return (
+    <>
+      <div>
+        <h1>팝업 테스트</h1>
+        <button onClick={openPopup}>팝업 열기</button>
+        {isOpen && (
+          <div>
+            <h2>팝업 제목</h2>
+            <p>팝업 내용 부분</p>
+            <button onClick={closePopup}>닫기</button>
+          </div>
+        )}
+
+      </div>
+    </>
+  );
+};
+
+export default App
