67f7077987048054400d551271c583ba5003bdb8
groomigroom <googjohn0143@gmail.com>
2026-08-09 21:48:28 +0900

    Update App.tsx

--- 용어사전에추가/useEffect강좌2번/App.tsx
+++ 용어사전에추가/useEffect강좌2번/App.tsx
@@ -1,10 +1,13 @@
 import {useState, useEffect} from 'react';
 
+import Timer from './Timer';
+
 function App() {
    
 
     return (
         <div>
+            <Timer/>
             <button>Toggle Timer</button>
         </div>
     );
