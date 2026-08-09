f3c8c88d0a7068967238124871186f5306e4ade1
groomigroom <googjohn0143@gmail.com>
2026-08-09 17:07:36 +0900

    Create App.tsx

---
+++ doruri_routing_title_change/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesSetup from './RoutesSetup';
+
+import "./App.css";
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <RoutesSetup />
+    </BrowserRouter>
+  )
+}
+
+export default App
