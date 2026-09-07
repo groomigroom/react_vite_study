485069ef5ab205724eaa75246dff8cf2ecf5ae04
groomigroom <googjohn0143@gmail.com>
2026-09-07 09:09:20 +0900

    Create App.tsx

---
+++ 이미지_객체/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { IMAGES } from './img_gac/images'
+
+function App() {
+
+  return (
+    <>
+      <img src={IMAGES.groomi1} alt="구름이1" />
+      <img src={IMAGES.groomi2} alt="구름이2" />
+      <img src={IMAGES.groomi3} alt="구름이3" />
+
+    </>
+  )
+}
+
+export default App
